e02f5f6268a22ae38a55e73e3303082b764b1af0
saka-21 <kasaikasai572@gmail.com>
2020-05-21 03:27:11 +0900

    Playerクラスの作成（途中）

--- babanuki_dir/models/babanuki_new.py
+++ babanuki_dir/models/babanuki_new.py
@@ -51,13 +51,36 @@ class Dealer(CreateCard):
         self.players_cards = players_cards
         for (name, player) in zip(self.player_names, self.players_cards):
             print(name + " : " + ",  ".join(player))
+        self.dict_deck = dict(zip(self.player_names, self.players_cards))
 
     # ペアがあれば捨ててください
     def delete_pair(self):
         print('\n=== Discard if you have pairs. ===')
 
-# class Player(object):
-#      def pull(self):
+
+class Player(Dealer):
+    def __init__(self, index, dealer):
+        self.name = dealer.player_names[index]
+        self.cards = dealer.dict_deck[self.name]
+        # print(self.name, ':', self.cards)
+
+
+    # def pull(self, next_player):
+    #     self.cards.append(next_player.cards.pop())
+
+
+    def delete_dup(self, dealer):
+        # 偶数枚の場合
+        only_num_list = []
+        for t in self.cards:
+            only_num_list.append(dealer.all_dict[t])
+        for x in set(only_num_list):
+            if only_num_list.count(x) == 2 or only_num_list.count(x) == 4:
+                only_num_list = [a for a in only_num_list if a != x]
+        # 3枚の場合
+        only_num_list = set(only_num_list)
+        only_num_list = list(only_num_list)
+        return only_num_list
 
 
 
@@ -71,6 +94,15 @@ class Game(CreateCard):
     dealer.what_is_your_name()
     dealer.deal()
     dealer.delete_pair()
+    for i in range(dealer.num):
+        player = Player(i, dealer)
+        a = player.delete_dup(dealer)
+        print(dealer.player_names[i], ':', a)
 
-Game()
+    def _show_result(self):
+
+
+
+if __name__ == '__main__':
+    Game()
 
