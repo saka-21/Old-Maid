92dd9a4a60576c4647fd7a93c770890e0a34e9de
saka-21 <kasaikasai572@gmail.com>
2020-05-22 20:59:35 +0900

    関数delete_dupの更新

--- babanuki_dir/models/babanuki_new.py
+++ babanuki_dir/models/babanuki_new.py
@@ -11,8 +11,9 @@ class CreateCard(object):
                 all_cards[key] = i
         all_cards['BABA'] = 0
         self.all_dict = all_cards
+        # print(self.all_dict)
         self.all_list = list(all_cards)
-
+        # print(self.all_list)
 
 class Dealer(CreateCard):
     def __init__(self):
@@ -53,8 +54,9 @@ class Dealer(CreateCard):
             print(name + " : " + ",  ".join(player))
         self.dict_deck = dict(zip(self.player_names, self.players_cards))
 
-    # ペアがあれば捨ててください
-    def delete_pair(self):
+    # ペアがあれば捨ててくださ園主問題
+    @staticmethod
+    def delete_pair():
         print('\n=== Discard if you have pairs. ===')
 
 
@@ -62,7 +64,12 @@ class Player(Dealer):
     def __init__(self, index, dealer):
         self.name = dealer.player_names[index]
         self.cards = dealer.dict_deck[self.name]
-        # print(self.name, ':', self.cards)
+        print(self.name, ':', self.cards)
+        player_dict = {}
+        for i in self.cards:
+            player_dict[i] = dealer.all_dict[i]
+            self.player_dict = player_dict
+        print('self.player_dict: ', self.player_dict)
 
 
     # def pull(self, next_player):
@@ -70,14 +77,41 @@ class Player(Dealer):
 
 
     def delete_dup(self, dealer):
-        # 偶数枚の場合
+        # 2, 4枚の場合
         only_num_list = []
         for t in self.cards:
             only_num_list.append(dealer.all_dict[t])
+        l = []
         for x in set(only_num_list):
             if only_num_list.count(x) == 2 or only_num_list.count(x) == 4:
-                only_num_list = [a for a in only_num_list if a != x]
+                for a in only_num_list:
+                    if a == x:
+                        l.append(a)
+        print('l:', l)
+        k = []
+        for i in set(l):
+            k.append([k for k, v in self.player_dict.items() if v == i])
+        print('k:', k)
+        for j in k:
+            for u in j:
+                del self.player_dict[u]
+        print('self.player_dict: ', self.player_dict)
+        print('set(only_num_list): ', set(only_num_list))
+
         # 3枚の場合
+        ju = []
+        for j in set(only_num_list):
+            ju.append([k for k, v in self.player_dict.items() if v == j])
+        print('ju: ', ju)
+        je = []
+        for i in ju:
+            if i:
+                je.append(i.pop())
+        print('je: ', je)
+
+
+
+
         only_num_list = set(only_num_list)
         only_num_list = list(only_num_list)
         return only_num_list
@@ -87,22 +121,31 @@ class Player(Dealer):
 
 
 class Game(CreateCard):
+    def play(self):
+        dealer = Dealer()
+        dealer.start()
+        dealer.how_many()
+        dealer.what_is_your_name()
+        dealer.deal()
+        dealer.delete_pair()
+        for i in range(dealer.num):
+            l = []
+            player = Player(i, dealer)
+            only_num = player.delete_dup(dealer)
+            print('only_num: ', only_num)
+            for j in only_num:
+                l.append([k for k, v in player.player_dict.items() if v == j])
+            print(l)
 
-    dealer = Dealer()
-    dealer.start()
-    dealer.how_many()
-    dealer.what_is_your_name()
-    dealer.deal()
-    dealer.delete_pair()
-    for i in range(dealer.num):
-        player = Player(i, dealer)
-        a = player.delete_dup(dealer)
-        print(dealer.player_names[i], ':', a)
-
-    def _show_result(self):
+    #         for k in a:
+    #             self.all_dict[]
+    #         self._show_result(dealer, i, a)
+    #
+    # def _show_result(self, dealer, i, contents):
+    #     print(dealer.player_names[i], ':', contents)
 
 
 
 if __name__ == '__main__':
-    Game()
-
+    game = Game()
+    game.play()
